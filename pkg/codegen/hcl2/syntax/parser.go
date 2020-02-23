13a407d5697c081d6420425716491a25334c3c51
Pat Gavlin <pat@pulumi.com>
2020-02-22 20:21:55 -0800

    Parser: comments

--- pkg/codegen/hcl2/syntax/parser.go
+++ pkg/codegen/hcl2/syntax/parser.go
@@ -1,3 +1,17 @@
+// Copyright 2016-2020, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package syntax
 
 import (
@@ -8,22 +22,26 @@ import (
 	"github.com/hashicorp/hcl/v2/hclsyntax"
 )
 
+// File represents a single parsed HCL2 source file.
 type File struct {
-	Name  string
-	Body  *hclsyntax.Body
-	Bytes []byte
+	Name  string          // The name of the file.
+	Body  *hclsyntax.Body // The body of the parsed file.
+	Bytes []byte          // The raw bytes of the source file.
 }
 
+// Parser is a parser for HCL2 source files.
 type Parser struct {
-	Files []*File
-
-	diagnostics hcl.Diagnostics
+	Files       []*File         // The parsed files.
+	Diagnostics hcl.Diagnostics // The diagnostics, if any, produced during parsing.
 }
 
+// NewParser creates a new HCL2 parser.
 func NewParser() *Parser {
 	return &Parser{}
 }
 
+// ParseFile attempts to parse the contents of the given io.Reader as HCL2. If parsing fails, any diagnostics generated
+// will be added to the parser's diagnostics.
 func (p *Parser) ParseFile(r io.Reader, filename string) error {
 	src, err := ioutil.ReadAll(r)
 	if err != nil {
@@ -36,18 +54,16 @@ func (p *Parser) ParseFile(r io.Reader, filename string) error {
 		Body:  hclFile.Body.(*hclsyntax.Body),
 		Bytes: hclFile.Bytes,
 	})
-	p.diagnostics = append(p.diagnostics, diags...)
+	p.Diagnostics = append(p.Diagnostics, diags...)
 	return nil
 }
 
-func (p *Parser) Diagnostics() hcl.Diagnostics {
-	return p.diagnostics
-}
-
+// NewDiagnosticWriter creates a new diagnostic writer for the files parsed by the parser.
 func (p *Parser) NewDiagnosticWriter(w io.Writer, width uint, color bool) hcl.DiagnosticWriter {
 	return NewDiagnosticWriter(w, p.Files, width, color)
 }
 
+// NewDiagnosticWriter creates a new diagnostic writer for the given list of HCL2 files.
 func NewDiagnosticWriter(w io.Writer, files []*File, width uint, color bool) hcl.DiagnosticWriter {
 	fileMap := map[string]*hcl.File{}
 	for _, f := range files {
